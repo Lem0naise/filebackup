83ccf3ea35bff1d298115ed4b1b83ebbbadc2fd0
Zac Nolan <zac1i1nolan@gmail.com>
2021-02-24 21:47:24 +0000

    Add files via upload

--- File_Backup.py
+++ File_Backup.py
@@ -1,4 +1,6 @@
 #importing stuff
+import datetime
+import win32com.client
 import tkinter as tkinter
 from tkinter import ttk
 from tkinter import *
@@ -10,9 +12,72 @@ import datetime
 import win32com.client
 
 def backup():
+	global mtw
 	#do backup stuff
 	print(delaymenu.get())
-	pass
+	if delaymenu.get() == "1 min":
+		mtw = 1
+	elif delaymenu.get() == "5 min":
+		mtw = 5
+	elif delaymenu.get() == "10 min":
+		mtw = 10
+	elif delaymenu.get() == "30 min":
+		mtw = 30
+	elif delaymenu.get() == "1 hour":
+		mtw = 60
+	elif delaymenu.get() == "6 hours":
+		mtw = 360
+	elif delaymenu.get() == "12 hours":
+		mtw = 720
+	elif delaymenu.get() == "1 day":
+		mtw = 1440
+	elif delaymenu.get() == "1 week":
+		mtw = 10080
+	elif delaymenu.get() == "1 month (30 days)":
+		mtw = 43200
+	elif delaymenu.get() == "1 year":
+		mtw = 525600
+
+	#adds backup task an dat
+	scheduler = win32com.client.Dispatch('Schedule.Service')
+	scheduler.Connect()
+	root_folder = scheduler.GetFolder('\\')
+	task_def = scheduler.NewTask(0)
+
+	# Create trigger
+	
+	#change minutes to whatever time selected
+	start_time = datetime.datetime.now() + datetime.timedelta(minutes=mtw)
+	TASK_TRIGGER_TIME = 1
+	trigger = task_def.Triggers.Create(TASK_TRIGGER_TIME)
+	trigger.StartBoundary = start_time.isoformat()
+
+	# Create action
+	TASK_ACTION_EXEC = 0
+	action = task_def.Actions.Create(TASK_ACTION_EXEC)
+	action.ID = 'File Backup'
+	action.Path = 'xcopy.exe'
+	#put stuff
+	action.Arguments = '""'
+
+	# Set parameters
+	task_def.RegistrationInfo.Description = 'Backup Folder'
+	task_def.Settings.Enabled = True
+	task_def.Settings.StopIfGoingOnBatteries = False
+
+	# Register task
+	# If task already exists, it will be updated
+	TASK_CREATE_OR_UPDATE = 6
+	TASK_LOGON_NONE = 0
+	root_folder.RegisterTaskDefinition(
+		'Backup Folder',  # Task name
+		task_def,
+		TASK_CREATE_OR_UPDATE,
+		'',  # No user
+		'',  # No password
+		TASK_LOGON_NONE)
+
+
 def start():
 	#setting window size
 	root = Tk()
